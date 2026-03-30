f262a5260a6f5915b5aecd0171ad25a1abc3c306
Diguinho <souza.rodrigues00@gmail.com>
2026-03-29 20:18:22 -0400

    Exercicios Completos ate o 12

--- Atividade POO/src/Carro.java
+++ Atividade POO/src/Carro.java
@@ -1,7 +1,57 @@
-    public class Carro {
-        String marca;
-        String modelo;
-        int ano;
+    class Carro {
+        private String marca;
+        private String modelo;
+        private int ano;
+
+        static int totalCarro = 0;
+
+        final int ANO_PRIMEIRO_CARRO = 1886;
+
+
+        public Carro(String marca, String modelo, int ano) {
+            this.marca = marca;
+            this.modelo = modelo;
+            setAno(ano);
+            totalCarro ++;
+        }
+
+       public Carro(){
+            this.marca = "Desconhecido";
+            this.modelo = "Desconhecido";
+            this.ano = 0;
+            totalCarro ++;
+       }
+
+
+        public String getMarca() {
+            return marca;
+        }
+
+        public void setMarca(String marca) {
+            this.marca = marca;
+        }
+
+        public String getModelo() {
+            return modelo;
+        }
+
+        public void setModelo(String modelo) {
+            this.modelo = modelo;
+        }
+
+        public int getAno() {
+            return ano;
+        }
+
+        public void setAno(int ano) {
+            this.ano = ano;
+            if(ano >= ANO_PRIMEIRO_CARRO){
+                this.ano = ano;
+            }
+            else{
+                System.out.println("Carro invalido, pois o primeiro carro foi inventado em: " + ANO_PRIMEIRO_CARRO);
+            }
+        }
 
         void exibir(){
             System.out.println("Marca: " + marca + " | Modelo: " + modelo + "| Ano: " + ano);
@@ -10,5 +60,8 @@
         int idadeCarro(){
             return 2026 - ano;
         }
+
+
+
 
     }
--- Atividade POO/src/Main.java
+++ Atividade POO/src/Main.java
@@ -1,37 +1,78 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        // Exercicio 01
 
-        Carro carro1 = new Carro();
-        carro1.marca = "Renault";
-        carro1.modelo = "Duster";
-        carro1.ano = 2023;
-        System.out.println("Marca: " + carro1.marca + " | Modelo: " + carro1.modelo + " | Ano: " + carro1.ano);
+        ArrayList<Carro> listaCarro = new ArrayList<>();
 
-        Carro carro2 = new Carro();
-        carro2.marca = "Ferrari";
-        carro2.modelo = "Spider";
-        carro2.ano = 2026;
-        System.out.println("Marca: " + carro2.marca + " | Modelo: " + carro2.modelo + " | Ano: " + carro2.ano);
+        System.out.println("----------------------------Exercicio 01 ---------------------------- ");
+
+
+        Carro carro1 = new Carro("Mustang", "Dark Horse", 1985);
+        listaCarro.add(carro1);
+        carro1.exibir();
+
+        System.out.println("Marca: " + carro1.getMarca() + " | Modelo: " + carro1.getModelo() + " | Ano: " + carro1.getAno());
+
+        Carro carro2 = new Carro("Ferrari", "Spider", 2026);
+        listaCarro.add(carro2);
+        carro2.exibir();
+
+        System.out.println("Marca: " + carro2.getMarca() + " | Modelo: " + carro2.getModelo() + " | Ano: " + carro2.getAno());
 
         //-----------------------------------------------------------------------
 
-        //Exercicio 02
+        System.out.println("----------------------------Exercicio 02 ---------------------------- ");
 
-        Carro carro3 = new Carro();
-        carro3.marca = "Lamborghini";
-        carro3.modelo = "Urus";
-        carro3.ano = 2020;
-
+        Carro carro3 = new Carro("Lamborghini", "Urus", 2020);
+        listaCarro.add(carro3);
         carro3.exibir(); // devolve o valor
 
         System.out.println("O 3º carro adicionado tem " + carro3.idadeCarro() + " anos de fabricado");
         //devolve uma ação
         //-----------------------------------------------------------------------
 
-        //Exercicio 03
+        System.out.println("----------------------------Exercicio 03 ---------------------------- ");
+        for(Carro carro : listaCarro){
+        carro.exibir();
+        }
+
+        System.out.println("----------------------------Exercicio 04 (FEITO USANDO ENCAPSULAMENTO)---------------------------- ");
+
+        System.out.println("----------------------------Exercicio 05 (FEITO USANDO CONSTRUTOR) ");
+        /*
+        Construtor é mais ágil, pois já faz uma pre-definição dos atributos
+        Já o get e o set, é preciso colocar o set no atributo, depois chamar no get.
+        */
+
+
+        System.out.println("----------------------------Exercicio 06 -----------------------------");
+        Carro carro4 = new Carro();
+        carro4.exibir();
+
+        // como criei um constutor e passei os atributos como "Desconhecido e 0" printa os valores colocados.
+
+        System.out.println("----------------------------Exercicio 07 -----------------------------");
+
+        Carro carro5 = new Carro("Ford", "Focus", 2019);
+        carro5.exibir();
+
+        System.out.println("Total de carros Cadastrados: " + Carro.totalCarro);
+        /* um atributo normal em cada classe, pertence excluisvamente aquele Objeto
+           Ja o STATIC ele pertence a Classe, engloba todos os Objetos.
+        */
+
+        System.out.println("----------------------------Exercicio 08 - FEITO (COMENTADO)-----------------------------");
+        /*
+        nao da certo porque static quer dizer a classe toda, ele consegue saber quantos carros foram criados
+        mas nao sabe o modelo, marca e ano, pois esses atributos é do objeto. DA ERRO DE COMPILAÇÃO.
+         */
+        System.out.println("----------------------------Exercicio 09 -----------------------------");
+
+        Carro carro6 = new Carro("Wolkswagem", "Fusca", 1883);
+        carro6.exibir();
+
     }
 
 
